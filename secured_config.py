8ceec97ea3e869d96e4a8777e3eadea8429199c5
Niriktelbot <110273551+Niriktelbot@users.noreply.github.com>
2022-11-29 21:24:26 +0600

    Add files via upload

---
+++ secured_config.py
@@ -0,0 +1,19 @@
+    # -*- coding: utf-8 -*-
+
+# Токен бота.
+TG_TOKEN = '5932262133:AAFCkI73IJUEOi0JpQ0CSWWw2biGRt_ijeA'
+
+# Не используются.
+ADMIN_QIWI = None
+QIWI_TOKEN = None
+
+
+MAIN_SETTINGS = {
+    "admins": ["GoldBloger"],       # Никнеймы админа для связи в телеграмм (без @).
+    "notification": 5504563793,  # ID для получения уведомлений.
+    "notif_settings": {
+        "checks": True,         # Уведомлять о проверках.
+        "transactions": True,   # Уведомлять о транзакциях.
+        "new": True             # Уведомлять о новых пользователях.
+    }
+}
